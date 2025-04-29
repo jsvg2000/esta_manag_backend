0b628bf9c80b4ddefa94c2ed749e23cc2cfb5770
Juan Sebastian Velasquez <juansebastian.velasquez@utp.edu.co>
2025-04-29 18:10:06 -0500

    delete initial file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MerchantsModule } from './merchants/merchants.module';
 import { SwaggerModule, DocumentBuilder  } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MerchantsModule);
 
   const config = new DocumentBuilder()
     .setTitle('API Adminitración de Establecimientos')
