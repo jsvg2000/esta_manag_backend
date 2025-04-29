9445d5496a4650b866478f701cf7ea3a069c973d
Juan Sebastian Velasquez <juansebastian.velasquez@utp.edu.co>
2025-04-29 13:25:04 -0500

    swanger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder  } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Adminitración de Establecimientos')
+    .setDescription('Administracion de establecimiento, cmerciantes y usuarios')
+    .setVersion('0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('doc-api',app,document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
